465773e731e89f5f70bded6594e470af23baaff3
‘swesnesna’ <balamanikandan368@gmail.com>
2021-09-18 11:35:27 +0530

    components created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BrandsComponent } from './brands/brands.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BrandsComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
